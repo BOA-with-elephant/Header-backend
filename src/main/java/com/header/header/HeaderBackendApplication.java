0905386cde7168dfb71605f1f40294738b196ee9
sortofel <yeoik731@gmail.com>
2025-07-03 16:18:33 +0900

    [CREATE] 프로젝트 세팅

---
+++ src/main/java/com/header/header/HeaderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.header.header;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeaderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeaderBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/header/header/HeaderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.header.header;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeaderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
