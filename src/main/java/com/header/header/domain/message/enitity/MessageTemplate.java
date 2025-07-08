da1dbd08d007323f2a898b4bf396a48ee97abc4d
Yeram522 <kyeram522@gmail.com>
2025-07-08 14:37:18 +0900

    [FIX] MessageTemplate entity, MessageTemplateDTO에 templateTitle 필드 추가

--- src/main/java/com/header/header/domain/message/enitity/MessageTemplate.java
+++ src/main/java/com/header/header/domain/message/enitity/MessageTemplate.java
@@ -15,10 +15,12 @@ public class MessageTemplate {
     @GeneratedValue( strategy = GenerationType.IDENTITY)
     private Integer templateCode;
     private Integer shopCode;
+    private String templateTitle;
     private String templateContent;
 
     @Enumerated(EnumType.STRING) // DB에 "INFORMATIONAL", "PROMOTIONAL" 문자열로 저장
     private TemplateType templateType;
+
 
     public void modifyMessageTemplateContent(String templateContent) {
         this.templateContent = templateContent;
