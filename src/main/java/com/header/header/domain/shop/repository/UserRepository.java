7ea53ed32a76bbd643e6aa4131d04727dfe4435e
sortofel <yeoik731@gmail.com>
2025-07-09 14:17:17 +0900

    [FEATURE] CRUD Shop - Map API entity import 구문 수정 closed #20

--- src/main/java/com/header/header/domain/shop/repository/UserRepository.java
+++ src/main/java/com/header/header/domain/shop/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.header.header.domain.shop.repository;
 
-import com.header.header.domain.user.enitity.User;
+import com.header.header.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // 유저 정보를 받아오기 위한 임시 처리.. 추후 머지 하면서 삭제될 예정
