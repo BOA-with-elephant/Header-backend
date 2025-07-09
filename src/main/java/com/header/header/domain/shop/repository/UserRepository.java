d95be751103fa17d5f471006dc6aee9d365a6bad
sortofel <yeoik731@gmail.com>
2025-07-09 14:44:13 +0900

    [BUG] User import 구문 수정

--- src/main/java/com/header/header/domain/shop/repository/UserRepository.java
+++ src/main/java/com/header/header/domain/shop/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.header.header.domain.shop.repository;
 
-import com.header.header.domain.user.enitity.User;
+import com.header.header.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // 임시로 해두고 정아님 거랑 합치기
