a26b2b5642db2a4045fc02fef82a58faba38c559
Yeram522 <kyeram522@gmail.com>
2025-07-10 11:43:01 +0900

    [STYLE] ShopMessageHistoryService 설계 주석 수정

--- src/main/java/com/header/header/domain/message/service/ShopMessageHistoryService.java
+++ src/main/java/com/header/header/domain/message/service/ShopMessageHistoryService.java
@@ -14,10 +14,12 @@ public class ShopMessageHistoryService {
 
     /* Creat */
     /* todo. 배치 Insert 이후 수신자 별로 history가 생성 됨. 메세지 발송과 비동기적으로 일어나야하는지 확인해야함! 예)번호가 없는 번호일 경우? 수신 차단된 경우? */
+    /* CREATE 할 때 STATUS = PENDING 으로 초기화 */
 
     /* Update */
     /* todo. 예약 메세지일 경우에는 reserved 상태에서 send로 변경됨. pending이 필요할 수도 있음! */
-    /* todo. 에러 메세지 관련??한 업데이트! */
+
+    /* todo. API 호출이 처리 된 후에 Update 함( STATUS나 ErrorMessage ) */
 
     /* Delete : ❌*/
 }
