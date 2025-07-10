73962dd6b71da6ff06fbf00b63050a27b7239fc7
Yeram522 <kyeram522@gmail.com>
2025-07-10 16:47:13 +0900

    [CREATE] 메세지 상태관리 Enum 추가 및 ShopMessageHistory Entity 수정

--- src/main/java/com/header/header/domain/message/entity/ShopMessageHistory.java
+++ src/main/java/com/header/header/domain/message/entity/ShopMessageHistory.java
@@ -1,5 +1,6 @@
 package com.header.header.domain.message.entity;
 
+import com.header.header.domain.message.enums.MessageStatus;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -22,10 +23,50 @@ public class ShopMessageHistory {
 
     @Lob
     private String msgContent;
-    private String sentStatus;
+
+    @Enumerated(EnumType.STRING)
+    private MessageStatus sentStatus;
+
     private String errorMessage;
     /* 예약 메세지일 경우에는 sentStatus가 reservation이었다가 sent로 바뀌게 된다 따라서 history의 Timestamp는 어노테이션으로
         관리하지 않는다. */
     private Timestamp sentAt;
+
+    public void updateStatus(MessageStatus newStatus, String errorMessage){
+        validateStatusTransition(newStatus);
+
+        this.sentStatus = newStatus;
+
+        if (newStatus == MessageStatus.SUCCESS) {
+            this.sentAt = new Timestamp(System.currentTimeMillis());
+            this.errorMessage = null; // 에러 메시지 초기화
+        }
+
+        // 실패 시 에러 메시지 설정
+        if (newStatus == MessageStatus.FAIL) {
+            this.errorMessage = errorMessage;
+        }
+
+    }
+
+    // 상태 전환 검증
+    private void validateStatusTransition(MessageStatus newStatus){
+        if(!this.sentStatus.canTransitionTo(newStatus)){
+            throw new IllegalStateException(
+                    String.format("상태전환불가: %s -> %s", this.sentStatus, newStatus)
+            );
+        }
+    }
+
+
+    // 편의 메서드들
+    public void markAsSuccess() {
+        updateStatus(MessageStatus.SUCCESS, null);
+    }
+
+    public void markAsFailed(String errorMessage) {
+        updateStatus(MessageStatus.FAIL, errorMessage);
+    }
+
 
 }
