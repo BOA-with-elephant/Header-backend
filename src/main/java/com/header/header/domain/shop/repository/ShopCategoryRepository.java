6ae8b4a1cdd90132c390533d74942b1f666dff5f
sortofel <yeoik731@gmail.com>
2025-07-14 09:33:29 +0900

    [FEATURE] Shop Category Read 기능 완료 (#62)
    closed #13

--- src/main/java/com/header/header/domain/shop/repository/ShopCategoryRepository.java
+++ src/main/java/com/header/header/domain/shop/repository/ShopCategoryRepository.java
@@ -3,5 +3,8 @@ package com.header.header.domain.shop.repository;
 import com.header.header.domain.shop.entity.ShopCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ShopCategoryRepository extends JpaRepository<ShopCategory, Integer> {
+    List<ShopCategory> findAllByOrderByCategoryCodeAsc();
 }
