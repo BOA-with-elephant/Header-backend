441c8eeceed68464d71776efd154ab3059394478
sortofel <yeoik731@gmail.com>
2025-07-15 13:55:41 +0900

    [FEATURE] Shop Holiday 테이블 생성에 맞춰 User Reservation 예외 처리 개선 closed #81

--- src/main/java/com/header/header/domain/reservation/service/UserReservationService.java
+++ src/main/java/com/header/header/domain/reservation/service/UserReservationService.java
@@ -5,6 +5,7 @@ import com.header.header.domain.menu.entity.Menu;
 import com.header.header.domain.menu.repository.MenuRepository;
 import com.header.header.domain.reservation.dto.UserReservationDTO;
 import com.header.header.domain.reservation.dto.UserReservationSearchConditionDTO;
+import com.header.header.domain.reservation.dto.UserResvAvailableScheduleDTO;
 import com.header.header.domain.reservation.entity.BossReservation;
 import com.header.header.domain.reservation.enums.ReservationState;
 import com.header.header.domain.reservation.enums.UserReservationErrorCode;
@@ -17,13 +18,17 @@ import com.header.header.domain.shop.entity.ShopHoliday;
 import com.header.header.domain.shop.repository.ShopHolidayRepository;
 import com.header.header.domain.shop.repository.ShopRepository;
 import com.header.header.domain.user.entity.User;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Date; // util.Date -> 날짜 및 시간 (1970 기준 밀리초 포함), 오래된 범용 타입, 사용 지양
+import java.sql.Time;
 import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,7 +46,7 @@ public class UserReservationService {
     /*사용자가 자신의 예약 내역을 상세 조회할 경우*/
     public Optional<UserReservationDetail> readDetailByUserCodeAndResvCode(Integer userCode, Integer resvCode) {
 
-            /*사용자 정보가 존재하지 않을 경우 예외*/
+        /*사용자 정보가 존재하지 않을 경우 예외*/
         User user = userRepository.findById(userCode)
                 .orElseThrow(() -> new UserReservationExceptionHandler(UserReservationErrorCode.USER_NOT_FOUND));
 
@@ -60,8 +65,7 @@ public class UserReservationService {
 
     /*사용자가 자신이 예약한 내역들의 목록을 조회할 경우*/
     public List<UserReservationSummary> findResvSummaryByUserCode(
-            UserReservationSearchConditionDTO conditionDTO)
-    {
+            UserReservationSearchConditionDTO conditionDTO) {
         /*유효성 체크가 된 데이터들 한개씩 꺼내오기*/
         Integer userCode = conditionDTO.getUserCode();
         Date startDate = conditionDTO.getStartDate();
@@ -75,7 +79,6 @@ public class UserReservationService {
                 throw new UserReservationExceptionHandler(UserReservationErrorCode.INPUT_DATE_WRONG);
             }
         }
-
 
         /*사용자 정보가 존재하지 않을 경우 예외*/
         User user = userRepository.findById(userCode)
@@ -94,10 +97,27 @@ public class UserReservationService {
     public Optional<UserReservationDetail> createReservation(
             UserReservationDTO dto
     ) {
-        /* 엔티티 유효성 검사*/
+        /* DTO 에서 어노테이션 유효성 검사 끝낸 값들 가져오기*/
         Integer userCode = dto.getUserCode();
         Integer shopCode = dto.getShopCode();
         Integer menuCode = dto.getMenuCode();
+        Date resvDate = dto.getResvDate();
+        Time resvTime = dto.getResvTime();
+
+        // 예약 시도 날짜가 샵 휴무일인 경우 예외
+        if (isHoliday(shopCode, resvDate)) {
+            throw new UserReservationExceptionHandler(UserReservationErrorCode.DATE_HAS_HOL);
+        }
+
+        // 예약 시도 날짜가 이미 예약되어 있을 경우 예외
+        if (!userReservationRepository.isAvailableSchedule(shopCode, resvDate, resvTime)) {
+            throw new UserReservationExceptionHandler(UserReservationErrorCode.SCHEDULE_ALREADY_TAKEN);
+        }
+
+        // 고객의 일정에 해당 날짜, 시간의 예약이 있을 경우 (노쇼 방지)
+        if(userReservationRepository.isUserHasReservationInThisSchedule(userCode, resvDate, resvTime)) {
+            throw new UserReservationExceptionHandler(UserReservationErrorCode.USER_SCHEDULE_UNAVAILABLE);
+        }
 
         /*유효하지 않은 사용자 정보일 경우 예외*/
         User user = userRepository.findById(userCode)
@@ -120,8 +140,8 @@ public class UserReservationService {
                 .userInfo(user)
                 .shopInfo(shop)
                 .menuInfo(menu)
-                .resvDate(dto.getResvDate())
-                .resvTime(dto.getResvTime())
+                .resvDate(resvDate)
+                .resvTime(resvTime)
                 .userComment(dto.getUserComment())
                 .resvState(ReservationState.APPROVE)
                 // 예약을 생성할 경우 기본값은 "예약확정"
@@ -192,6 +212,76 @@ public class UserReservationService {
 
         /* 휴일 아님 false 반환*/
         return false;
+    }
+
+    /* 프론트에 예약 가능한 날짜와 시간을 모아서 보내주는 용도 */
+    // @Param: shopCode, dateRangeToGet (for 문에서 스캔할 날짜의 개수... 일단 한 달 줄 예정)
+    @Transactional(readOnly = true)
+    // 영속성 컨텍스트에게 읽기 전용이라는 것을 명시해 성능 향상. 날짜별로 for문을 돌려서 쿼리가 많이 실행되기 때문에 방어용도로 사용
+    public List<UserResvAvailableScheduleDTO> getAvailableSchedule(Integer shopCode, int dateRangeToGet) {
+
+        //존재하지 않는 샵일 경우 예외
+        Shop shop = shopRepository.findById(shopCode)
+                .orElseThrow(() -> new UserReservationExceptionHandler(UserReservationErrorCode.SHOP_NOT_FOUND));
+
+        List<UserResvAvailableScheduleDTO> result = new ArrayList<>();
+
+        /* 데이터 검증 및 추가 순서
+          @Param: shopOpen, shopClose, dateRangeToGet
+        * 1) 현재를 기준으로 미래 한 달(dateRangeToGet) 까지 스캔해서 휴일이 없는 날짜
+             - 현재 날짜+for문 돌리는 날짜, 샵 코드 필요
+             - if(true) continue (휴일이 있다면, 현재 반복문 중단하고 다시 돌아가 조건에 맞으면 다음 단계 시행)
+
+          2) 1 통과한 날짜들의 시간 한 시간 단위로 쪼개기
+
+          3) 2 통과한 날짜, 시간 중 예약자가 없는 날짜와 시간
+             - 1에서 통과한 날짜, 쪼개진 시간 필요
+             - if (isAvailableSchedule == true) 인 경우만 list 추가
+        * */
+
+        for (int i = 0; i < dateRangeToGet; i++) {
+
+            //for 문의 기준이 될 targetDate
+            LocalDate targetDate = LocalDate.now().plusDays(i);
+
+            // targetDate가 휴일인 경우 continue로 건너뛰고 다음 날짜 검증
+            if (isHoliday(shopCode, Date.valueOf(targetDate))) continue;
+
+            // 예약 가능한 시간들을 담을 list
+            List<LocalTime> availableTimes = new ArrayList<>();
+
+            /* String 타입인 샵 운영 시간 가져오기,
+                shopOpen : 더해줄 시간 시작점
+                shopClose : 시간 더하기 종료지점
+                for문 내부에서 LocalDate.now(), plusMinutes() 사용하기 위해 LocalTime 사용 */
+            LocalTime time = LocalTime.parse(shop.getShopOpen());
+            LocalTime shopClose = LocalTime.parse(shop.getShopClose());
+
+            // time 시간이 close 시간보다 이전일 동안
+            while (time.isBefore(shopClose)) {
+
+                // 예약 가능한 시간인지 검증
+                boolean isAvailableSchedule = userReservationRepository
+                        .isAvailableSchedule(shopCode, Date.valueOf(targetDate), Time.valueOf(time));
+
+                if (isAvailableSchedule) {
+                    // 예약 가능한 시간인 경우, 가능한 시간 list에 하나씩 더해준다
+                    availableTimes.add(time);
+                }
+                // 예약은 한 시간 단위로 받는다
+                time = time.plusMinutes(60);
+
+            }
+                // while 문을 빠져나왔을 때
+                // 가능한 시간을 담은 리스트가 비어있지 않을 때만 UserResvAvailableScheduleDTO 객체 생성
+                if (!availableTimes.isEmpty()) {
+                    result.add(new UserResvAvailableScheduleDTO(targetDate, availableTimes));
+                }
+
+        }
+
+        // for문 빠져나온 result 저장
+        return result;
     }
 
 }
