d738ab95e46b73548cee41d938530859610d08ea
Yeram522 <kyeram522@gmail.com>
2025-07-16 15:33:32 +0900

    [CREATE] api/v1/my-shops/{shopId} 루트의 요청을 상위 컴포넌트에서 공통화 하기위한 추상클래스 생성

---
+++ src/main/java/com/header/header/common/controller/MyShopBaseController.java
@@ -0,0 +1,16 @@
+package com.header.header.common.controller;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("api/v1/my-shops/{shopId}")
+public abstract class MyShopBaseController {
+
+    protected Long shopId;
+
+    @ModelAttribute
+    public void setShopId(@PathVariable("shopId") Long shopId){
+        this.shopId = shopId;
+    }
+}
