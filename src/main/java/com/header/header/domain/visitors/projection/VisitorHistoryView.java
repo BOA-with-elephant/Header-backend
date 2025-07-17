eba24658958ecfd9650ee3559ff80a01cf0c7da5
Yeram522 <kyeram522@gmail.com>
2025-07-17 16:03:57 +0900

    [CREATE] 고객 방문 히스토리 뷰를 위한 projection interface 추가

---
+++ src/main/java/com/header/header/domain/visitors/projection/VisitorHistoryView.java
@@ -0,0 +1,9 @@
+package com.header.header.domain.visitors.projection;
+
+import java.time.LocalDate;
+
+public interface VisitorHistoryView {
+    LocalDate getVisitDate();
+    String getMenuName();
+    Integer getSalesPrice();
+}
