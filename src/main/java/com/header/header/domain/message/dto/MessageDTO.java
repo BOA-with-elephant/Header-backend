3a2a9450ba89dadc564950a66231722dc0fbe863
Nongdam <63442636+Yeram522@users.noreply.github.com>
2025-07-21 15:05:53 +0900

    [FEATURE] #75 send message service controller mapping (#116)
    * [STYLE] MessageRequest DTO 이름 수정 -> MessageDTO
    
    * [FIX] MessageDTO에서 messageTemplate 필드 삭제
    
    * [FIX] MessageDTO에서 수신자 필드를 리스트 타입으로 변경 및 TestCode와 Service 코드 수정
    
    * [FIX] 단체 메세지 전송 시 개별 메세지 전송 결과에 따른 batch의 전송결과 카운트 업데이트 로직 추가
    
    * [REFACTOR] 단체 메세지 발송 시 메세지 당 스레드 하나 생성 로직을 한 배치당 한 스레드로 수정
    
    * [FIX] MessageIntegrationTest 코드에 사용되는 매개변수 수정
    
    * [FEATURE] 단체 메세지 전송 Test 완료
    
    * [FEATURE] clientCode로 userCode 변환 쿼리 작성
    
    * [CREATE] MessageRequest DTO 생성
    
    * [CREATE] 메세지 즉시 발송 Facade 메서드 작성
    
    * [CREATE] MessageController 메세지 전송 API 작성 및 POSTMAN 테스트 성공

---
+++ src/main/java/com/header/header/domain/message/dto/MessageDTO.java
@@ -0,0 +1,29 @@
+package com.header.header.domain.message.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+/* Service에서 사용하는 메세지 DTO 입니다. */
+@Data
+@Builder
+public class MessageDTO {
+    @NotBlank(message = "유저 코드는 필수입니다")
+    private List<Integer> to;
+
+    @NotBlank(message = "발신 샵 코드는 필수입니다")
+    private Integer from;
+
+    @NotBlank(message = "전송 타입은 필수입니다.")
+    private String sendType;
+
+    @NotBlank(message = "제목은 필수 입니다.")
+    private String subject;
+
+    @NotBlank(message = "메시지 내용은 필수입니다")
+    @Size(max = 1000, message = "메시지는 1000자 이하여야 합니다")
+    private String text;
+}
