df3441ab55e50a2c0711877c369798785f90cbaa
21jayoon <21jayoon@gmail.com>
2025-07-21 20:31:24 +0900

    [FEATURE] user controller mapping and spring security apply      close #86

--- src/main/java/com/header/header/auth/exception/DuplicatedMemberEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.header.header.auth.exception;
-
-public class DuplicatedMemberEmailException extends RuntimeException {
-    public DuplicatedMemberEmailException(String message) {
-        super(message);
-    }
-}
