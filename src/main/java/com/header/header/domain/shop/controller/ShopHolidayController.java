989a76dd1b5817b978d72287f9aa953c6bbd53c0
sortofel <yeoik731@gmail.com>
2025-07-21 15:06:47 +0900

    [FIX] ShopHolidayController base url 수정

--- src/main/java/com/header/header/domain/shop/controller/ShopHolidayController.java
+++ src/main/java/com/header/header/domain/shop/controller/ShopHolidayController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/shops")
+@RequestMapping("/myshop/{shopCode}")
 @CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:3000"},
         allowedHeaders = "*",
-        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.DELETE})
+        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class ShopHolidayController {
 
     private final ShopHolidayService shopHolidayService;
