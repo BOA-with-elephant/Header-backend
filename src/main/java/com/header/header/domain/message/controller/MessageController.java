ce76ae0ff69817c012feebe02e98825034f461ea
Yeram522 <kyeram522@gmail.com>
2025-07-22 01:53:39 +0900

    [STYLE] MessageController EndPoint API Mapping Method 구조 작성 및 주석

--- src/main/java/com/header/header/domain/message/controller/MessageController.java
+++ src/main/java/com/header/header/domain/message/controller/MessageController.java
@@ -19,6 +19,12 @@ public class MessageController extends MyShopBaseController {
     private final MessageSendFacadeService messageSendFacadeService;
     private final MessageTemplateService messageTemplateService;
 
+    /**
+     * 메세지를 발송합니다.(즉시발송 or 예약발송)
+     * @param shopId 샵 코드
+     * @param requestBody 요청 본문
+     * @return MessageResponse
+     */
     @PostMapping("/messages")
     public ResponseEntity<ApiResponse<MessageResponse>> sendMessage(
             @PathVariable Integer shopId,
@@ -28,11 +34,16 @@ public class MessageController extends MyShopBaseController {
 
         if(!requestBody.getIsScheduled()){
             messageResponse =  messageSendFacadeService.sendImmediateMessage(requestBody);
-        }// todo. 예약 발송일 경우.
+        }// todo. 예약 발송일 경우. else ~
 
         return ResponseEntity.ok(ApiResponse.success(messageResponse));
     }
 
+    /**
+     * 정보성 템플릿 + 광고성 템플릿 리스트를 조회합니다.
+     * @param shopId 샵 코드
+     * @return List<MessageTemplateResponse>
+     */
     @GetMapping("/template")
     public ResponseEntity<ApiResponse<List<MessageTemplateResponse>>> getTemplateList(
             @PathVariable Integer shopId) {
@@ -40,5 +51,48 @@ public class MessageController extends MyShopBaseController {
 
         return ResponseEntity.ok(ApiResponse.success(response));
     }
+
+    /**
+     * 새로운 메세지 템플릿을 등록합니다.
+     * @param shopId 샵 코드
+     * @return String 등록 완료 문구
+     */
+    @PostMapping("/template")
+    public ResponseEntity<ApiResponse<List<MessageTemplateResponse>>> registTemplate(
+            @PathVariable Integer shopId) {
+
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    /**
+     * 메세지 템플릿 내용을 수정합니다.
+     * @param shopId 샵 코드
+     * @param templateCode 수정할 템플릿 코드
+     * @return String 수정 완료 문구
+     */
+    @PutMapping("/template/{templateCode}")
+    public ResponseEntity<ApiResponse<String>> modifyTemplateContent(
+            @PathVariable Integer shopId,
+            @PathVariable Integer templateCode
+    ){
+
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    /**
+     * 메세지 템플릿을 삭제합니다.(물리적 삭제)
+     * @param shopId 샵 코드
+     * @param templateCode 템플릿 코드
+     * @return String 삭제 완료 문구
+     */
+    @DeleteMapping("/template/{templateCode}")
+    public ResponseEntity<ApiResponse<String>> deleteTemplate(
+            @PathVariable Integer shopId,
+            @PathVariable Integer templateCode
+    ){
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+
 
 }
