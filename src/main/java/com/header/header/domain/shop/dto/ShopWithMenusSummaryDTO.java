2ee4c05c3581688dfc21e7308ed704f1f1c805be
sortofel <yeoik731@gmail.com>
2025-08-12 11:55:14 +0900

    [FIX] 프로젝션 dto로 수정

--- src/main/java/com/header/header/domain/shop/dto/ShopWithMenusSummaryDTO.java
+++ src/main/java/com/header/header/domain/shop/dto/ShopWithMenusSummaryDTO.java
@@ -1,18 +1,23 @@
 package com.header.header.domain.shop.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
-public interface ShopWithMenusSummaryDTO {
-    Integer getShopCode();
-    String getShopName();
-    String getShopPhone();
-    String getShopLocation();
-    Double getShopLong();
-    Double getShopLa();
-    String getCategoryName();
-    Double getDistance();
-    String getMenuName();
-    Integer getMenuRevCount();
+@Getter
+@Setter
+public class ShopWithMenusSummaryDTO {
+    private Integer shopCode;
+    private String shopName;
+    private String shopPhone;
+    private String shopLocation;
+    private Double shopLong;
+    private Double shopLa;
+    private String categoryName;
+    private Double distance;
+    private String menuName;
+    private Integer menuRevCount;
 
-    List<MenuSummaryDTO> getMenus();
+    private List<MenuSummaryDTO> menus;
 }
