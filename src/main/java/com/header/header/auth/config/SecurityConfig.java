e8ec5e63ead24e9e18e098aee67dadeed1128ad2
sortofel <yeoik731@gmail.com>
2025-08-12 14:13:27 +0900

    [FIX] 보안 허용에 shops 경로 추가 - 뒤에 슬래시(/)가 없으면 인식하지 못해 403 에러가 발생하는 문제 해결

--- src/main/java/com/header/header/auth/config/SecurityConfig.java
+++ src/main/java/com/header/header/auth/config/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
                     auth.requestMatchers("/api/v1/my-shops/**").permitAll();
 //                    auth.requestMatchers("/my-shops/**").hasRole("ADMIN");
                     // "/shops/**" 경로는 USER 역할, ADMIN역할 모두 접근 가능합니다.
-                    auth.requestMatchers("/api/v1/shops/**").permitAll();
+                    auth.requestMatchers("/api/v1/shops", "/api/v1/shops/**").permitAll();
 //                    auth.requestMatchers("/shops/**").hasAnyRole("ADMIN", "USER");
                     // 그 외 모든 요청은 인증된 사용자만 접근 가능합니다.
                     auth.anyRequest().authenticated();
