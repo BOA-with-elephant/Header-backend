c8880c131b4711a7820225bd9a11ca23b5b74dc5
sortofel <yeoik731@gmail.com>
2025-08-13 12:05:31 +0900

    [FIX] ShopErrorCode, UserReservationErrorCode에 HttpStatus 추가

--- src/main/java/com/header/header/domain/shop/enums/ShopErrorCode.java
+++ src/main/java/com/header/header/domain/shop/enums/ShopErrorCode.java
@@ -2,6 +2,7 @@ package com.header.header.domain.shop.enums;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
@@ -9,16 +10,17 @@ public enum ShopErrorCode {
 
     // Shop CRUD 시 발생할 수 있는 에러 코드를 관리하는 enum
 
-    NOT_SUPPORTED_REQUEST("NOT_SUPPORTED_REQUEST", "유효하지 않은 접근입니다."),
-    SHOP_NOT_FOUND("SHOP_NOT_FOUND", "해당 샵을 찾을 수 없습니다."),
-    SHOP_DEACTIVATED("SHOP_DEACTIVATED", "해당 샵은 비활성화 되었습니다."),
-    SHOP_ALREADY_DEACTIVATED("SHOP_ALREADY_DEACTIVATED", "해당 샵은 이미 비활성화 상태입니다."),
-    LOCATION_NOT_FOUND("LOCATION_NOT_FOUND", "잘못된 주소입니다."),
-    UNKNOWN_DB_VALUE("UNKNOWN_DB_VALUE", "유효하지 않은 값입니다."),
-    ADMIN_NOT_FOUND("ADMIN_NOT_FOUND", "유효하지 않은 관리자 정보입니다."),
-    SHOP_CATEGORY_NOT_FOUND("SHOP_CATEGORY_NOT_FOUND", "유효하지 않은 카테고리 입니다."),
-    SHOP_IS_EMPTY("SHOP_IS_EMPTY", "보유한 샵이 존재하지 않습니다.");
+    NOT_SUPPORTED_REQUEST("NOT_SUPPORTED_REQUEST", "유효하지 않은 접근입니다.", HttpStatus.BAD_REQUEST),
+    SHOP_NOT_FOUND("SHOP_NOT_FOUND", "해당 샵을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    SHOP_DEACTIVATED("SHOP_DEACTIVATED", "해당 샵은 비활성화 되었습니다.", HttpStatus.GONE),
+    SHOP_ALREADY_DEACTIVATED("SHOP_ALREADY_DEACTIVATED", "해당 샵은 이미 비활성화 상태입니다.", HttpStatus.CONFLICT),
+    LOCATION_NOT_FOUND("LOCATION_NOT_FOUND", "잘못된 주소입니다.", HttpStatus.NOT_FOUND),
+    UNKNOWN_DB_VALUE("UNKNOWN_DB_VALUE", "유효하지 않은 값입니다.", HttpStatus.BAD_REQUEST),
+    ADMIN_NOT_FOUND("ADMIN_NOT_FOUND", "유효하지 않은 관리자 정보입니다.", HttpStatus.NOT_FOUND),
+    SHOP_CATEGORY_NOT_FOUND("SHOP_CATEGORY_NOT_FOUND", "유효하지 않은 카테고리 입니다.", HttpStatus.NOT_FOUND),
+    SHOP_IS_EMPTY("SHOP_IS_EMPTY", "보유한 샵이 존재하지 않습니다.", HttpStatus.NOT_FOUND);
 
     private final String code;
     private final String message;
+    private final HttpStatus httpStatus;
 }
