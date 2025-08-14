31de583c5db78a630e9c2fc6a231436110ff3506
sortofel <yeoik731@gmail.com>
2025-08-14 11:18:59 +0900

    [FIX] 커스텀 캐시 설정 및 등록

--- src/main/java/com/header/header/config/CacheConfig.java
+++ src/main/java/com/header/header/config/CacheConfig.java
@@ -15,12 +15,21 @@ public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager(){
-        CaffeineCacheManager cacheManager
-                = new CaffeineCacheManager("holidays", "reserved-schedule");
-        Caffeine.newBuilder()
-                .expireAfterWrite(1, TimeUnit.DAYS)
-                .maximumSize(1000) // TODO. 사이즈 설정 최적화 필요
-                .build();
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+
+        // holidays 캐시 설정
+        cacheManager.registerCustomCache("holidays",
+                Caffeine.newBuilder()
+                        .expireAfterWrite(48, TimeUnit.HOURS) // 휴일 정보는 자주 바뀌지 않음
+                        .maximumSize(3000) // 100개 가게의 30일치 휴일 정보 저장
+                        .build());
+
+        // reserved-schedule 캐시 설정
+        cacheManager.registerCustomCache("reserved-schedule",
+                Caffeine.newBuilder()
+                        .expireAfterWrite(24, TimeUnit.HOURS) // 예약 정보는 매일 바뀜
+                        .maximumSize(10000) // 영업 시간이 9~18 인 가게 40개의 예약 정보 저장
+                        .build());
 
         return cacheManager;
     }
