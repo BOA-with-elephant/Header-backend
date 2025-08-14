d788cce9c2007d4e34c7f1624d0eabea8989a8ce
sortofel <yeoik731@gmail.com>
2025-08-14 10:58:51 +0900

    [FIX] CacheConfig 사용자화 - 쓰기 후 만료 시간 설정 - 최대 사이즈 설정 (수정 필요)

--- src/main/java/com/header/header/config/CacheConfig.java
+++ src/main/java/com/header/header/config/CacheConfig.java
@@ -1,10 +1,13 @@
 package com.header.header.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
@@ -14,6 +17,10 @@ public class CacheConfig {
     public CacheManager cacheManager(){
         CaffeineCacheManager cacheManager
                 = new CaffeineCacheManager("holidays", "reserved-schedule");
+        Caffeine.newBuilder()
+                .expireAfterWrite(1, TimeUnit.DAYS)
+                .maximumSize(1000) // TODO. 사이즈 설정 최적화 필요
+                .build();
 
         return cacheManager;
     }
