8af679a69ad2e0089f4e11e097e5a68c830faf2e
sortofel <yeoik731@gmail.com>
2025-08-14 13:48:37 +0900

    [REFACTOR] #193 예약 가능 시간 캐시 처리 (#204)
    * [FIX] Redis 의존성 추가
    
    * [FIX] RedisCacheManager 설정 클래스 추가
    
    * [FIX] 어플리케이션에 @EnableCaching 추가
    
    * [FEAT] isHoliday 메서드 캐시 추가
    - 캐싱 대상 메서드 자기 참조 방지 필요 -> 메서드 분리
    - 캐싱 어노테이션 추가
    
    * [FEAT] reserved-schedule 캐시 추가
    
    * [FIX] 로그 삭제
    
    * [FEAT] 예약 삭제, 취소 시 캐싱 처리 추가
    
    * [FEAT] 예약 삭제, 취소 시 캐싱 처리 추가
    
    * [FEAT] 특정 샵에 대한 휴일 캐시 삭제 메소드 추가, 휴일 생성/수정/삭제 시 적용
    
    * [FIX] RedisTemplate 설정
    
    * [FIX] Redis 방식 Local로 변경
    
    * [FIX] - 사용하지 않는 import 삭제
    - Caffeine 캐시 매니저 -> 기본 캐시 매니저로 수정 (Bean 등록 문제)
    - 캐시 생성/삭제 테스트 완료
    
    * [FIX] CacheConfig 사용자화
    - 쓰기 후 만료 시간 설정
    - 최대 사이즈 설정 (수정 필요)
    
    * [FIX] 캐시테스트 변수명 변경 (snake -> camel)
    
    * [FIX] 커스텀 캐시 설정 및 등록
    
    * [FIX] 이미 검증된 예약 취소 로그 삭제
    
    * [FIX] 캐시 이름 변경 (reserved-schedule -> available-schedule)
    
    * [FIX] 캐시 테스트 수정
    - 수 직접 작성 대신 변수 사용
    - 캐시 생성 테스트 추가

---
+++ src/main/java/com/header/header/config/CacheConfig.java
@@ -0,0 +1,37 @@
+package com.header.header.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager(){
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+
+        // holidays 캐시 설정
+        cacheManager.registerCustomCache("holidays",
+                Caffeine.newBuilder()
+                        .expireAfterWrite(48, TimeUnit.HOURS) // 휴일 정보는 자주 바뀌지 않음
+                        .maximumSize(3000) // 100개 가게의 30일치 휴일 정보 저장
+                        .build());
+
+        // available-schedule 캐시 설정
+        cacheManager.registerCustomCache("available-schedule",
+                Caffeine.newBuilder()
+                        .expireAfterWrite(24, TimeUnit.HOURS) // 예약 정보는 매일 바뀜
+                        .maximumSize(10000) // 영업 시간이 9~18 인 가게 40개의 예약 정보 저장
+                        .build());
+
+        return cacheManager;
+    }
+
+}
