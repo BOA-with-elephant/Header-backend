bfe151429a4039c97379e8e2b7f8b485e6d09c29
JUHYE KIM <rhkdlf48301@gmail.com>
2025-08-28 10:45:03 +0900

    edit webmvcconfig (#300)

--- src/main/java/com/header/header/config/WebMvcConfig.java
+++ src/main/java/com/header/header/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.header.header.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,7 +15,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>>  converters) {
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setDefaultCharset(StandardCharsets.UTF_8);
+        converter.setDefaultCharset(StandardCharsets.UTF_8);converter.setSupportedMediaTypes(List.of(
+                new MediaType("application", "json", StandardCharsets.UTF_8)
+        ));
         converters.add(converter);
     }
 }
